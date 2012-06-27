519db6d4565fdca785b2accbf9cb5e7aa1585dbd
andrew giessel <andrew.giessel@gmail.com>
2012-06-27 10:09:21 -0400

    Changes to allow for document sizes of 512mb

--- src/mongo/bson/util/builder.h
+++ src/mongo/bson/util/builder.h
@@ -46,7 +46,7 @@ namespace mongo {
          update $push (append) operation
          various db.eval() type operations
     */
-    const int BSONObjMaxUserSize = 16 * 1024 * 1024;
+    const int BSONObjMaxUserSize = 512 * 1024 * 1024;
 
     /*
        Sometimes we need objects slightly larger - an object in the replication local.oplog
